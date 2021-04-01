708c30ae96bf8d0b1cb76a5008bed1ff98e15e1b
Shery-Bukhari <Sheharyaar246@gmail.com>
2021-04-01 10:01:00 +0500

    Initial project setup

--- src/config/database.js
+++ src/config/database.js
@@ -10,4 +10,4 @@ const connectDB = async () => {
     }
 };
 
-module.exports = { connectDB };\n// Updated: 2025-05-30 16:01:23
+module.exports = { connectDB };\n// Updated: 2025-05-30 16:01:23\n// Updated: 2025-05-30 16:01:23
