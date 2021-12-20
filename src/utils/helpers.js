8c38350b2ed633038a2a6975da366ac2259fa630
Shery-Bukhari <Sheharyaar246@gmail.com>
2021-12-20 11:52:00 +0500

    Update test coverage

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -32,4 +32,4 @@ module.exports = {
     validateEmail,
     generateId,
     debounce
-};\n// Updated: 2025-05-30 16:01:23\n// Updated: 2025-05-30 16:01:24\n// Updated: 2025-05-30 16:01:25
+};\n// Updated: 2025-05-30 16:01:23\n// Updated: 2025-05-30 16:01:24\n// Updated: 2025-05-30 16:01:25\n// Updated: 2025-05-30 16:01:26
